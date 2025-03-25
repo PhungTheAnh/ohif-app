418cac389f46bdaf5753f68ac805723f134c8239
anhpt <phungtheanh2001@gmail.com>
2025-03-25 10:54:29 +0700

    first commit

---
+++ extensions/measurement-tracking/src/panels/PanelMeasurementTableTracking.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { utils } from '@ohif/core';
+import { AccordionTrigger, MeasurementTable, useViewportGrid } from '@ohif/ui-next';
+import {
+  PanelMeasurement,
+  StudyMeasurements,
+  StudyMeasurementsActions,
+  StudySummaryFromMetadata,
+  AccordionGroup,
+  MeasurementsOrAdditionalFindings,
+} from '@ohif/extension-cornerstone';
+
+import { useTrackedMeasurements } from '../getContextModule';
+
+const { filterAnd, filterPlanarMeasurement, filterMeasurementsBySeriesUID } =
+  utils.MeasurementFilters;
+
+function PanelMeasurementTableTracking(props) {
+  const [viewportGrid] = useViewportGrid();
+  const [trackedMeasurements, sendTrackedMeasurementsEvent] = useTrackedMeasurements();
+  const { trackedStudy, trackedSeries } = trackedMeasurements.context;
+  const measurementFilter = trackedStudy
+    ? filterAnd(filterPlanarMeasurement, filterMeasurementsBySeriesUID(trackedSeries))
+    : filterPlanarMeasurement;
+
+  const EmptyComponent = () => (
+    <MeasurementTable
+      title="Measurements"
+      isExpanded={false}
+    >
+      <MeasurementTable.Body />
+    </MeasurementTable>
+  );
+
+  const actions = {
+    createSR: ({ StudyInstanceUID }) => {
+      sendTrackedMeasurementsEvent('SAVE_REPORT', {
+        viewportId: viewportGrid.activeViewportId,
+        isBackupSave: true,
+        StudyInstanceUID,
+        measurementFilter,
+      });
+    },
+  };
+
+  const Header = props => (
+    <AccordionTrigger
+      asChild={true}
+      className="px-0"
+    >
+      <div data-cy="TrackingHeader">
+        <StudySummaryFromMetadata {...props} />
+      </div>
+    </AccordionTrigger>
+  );
+
+  return (
+    <PanelMeasurement
+      measurementFilter={measurementFilter}
+      emptyComponent={EmptyComponent}
+      sourceChildren={props.children}
+    >
+      <StudyMeasurements grouping={props.grouping}>
+        <AccordionGroup.Trigger
+          key="trackingMeasurementsHeader"
+          asChild={true}
+        >
+          <Header key="trackingHeadChild" />
+        </AccordionGroup.Trigger>
+        <MeasurementsOrAdditionalFindings
+          key="measurementsOrAdditionalFindings"
+          activeStudyUID={trackedStudy}
+          customHeader={StudyMeasurementsActions}
+          measurementFilter={measurementFilter}
+          actions={actions}
+        />
+      </StudyMeasurements>
+    </PanelMeasurement>
+  );
+}
+
+export default PanelMeasurementTableTracking;
